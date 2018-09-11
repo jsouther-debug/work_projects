fc9714a0775ddafc346facbc1327c9c8c8fd413b
jared_souther@taylor.edu <jsouther@cse21703.cs.local>
2018-09-11 18:46:06 -0400

    milestone 2

--- course/cos232/project/steganography/encoder.c
+++ course/cos232/project/steganography/encoder.c
@@ -10,4 +10,19 @@ int main(int argc, char **argv) {
     printf ("output file: %s\n", outputf);
     printf ("message: %s\n", message);
 
+    signed int start;
+    fseek (infile, 10, SEEK_SET);
+    fread (&start, 4,1, infile);
+    printf("starting position: %d\n", start);
+
+    signed int width;
+    fseek (infile ,18, SEEK_SET);
+    fread (&width, 4, 1, infile);
+    printf("width: %d\n", width);
+
+    signed int height;
+    fseek (infile, 22, SEEK_SET);
+    fread (&height, 4,1, infile);
+    printf("height: %d\n", height);
+
 }
