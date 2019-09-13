f05ffb0bae2e1e587d6196f34cccf76fded7d196
Jonathan Geisler <jgeisler@cse.taylor.edu>
2019-09-12 23:23:00 -0400

    Downgrade MOCK_METHOD to legacy API
    The version installed on CSE Ubuntu machines uses the legacy API, so
    make sure it is supported.

--- course/cos381/hw0/LogicUnitTest.h
+++ course/cos381/hw0/LogicUnitTest.h
@@ -7,13 +7,21 @@
 template<int N>
 class MockListener : public Listener<N> {
   public:
+#ifdef MOCK_METHOD
     MOCK_METHOD(void, update_input, (int num, Value<N> value), (override));
+#else
+    MOCK_METHOD2_T(update_input, void(int num, Value<N> value));
+#endif
 };
 
 template<int N>
 class MockReporter : public Reporter<N> {
   public:
+#ifdef MOCK_METHOD
     MOCK_METHOD(Value<N>, get_output, (), (const override));
+#else
+    MOCK_CONST_METHOD0_T(get_output, Value<N>());
+#endif
 };
 
 #endif
