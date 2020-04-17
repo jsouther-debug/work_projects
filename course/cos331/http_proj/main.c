b8560aec199ad8fa3bd5a97807fbac5e3d8477b1
jsouther-debug <59623910+jsouther-debug@users.noreply.github.com>
2020-04-17 16:53:42 -0400

    Update main.c

--- course/cos331/http_proj/main.c
+++ course/cos331/http_proj/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <errno.h>
 #include <netdb.h>
@@ -12,14 +13,15 @@
 
 #define MYHOST "localhost"
 #define MYPORT "8080"
-#define MAXTHREADS 1000
 #define BYTES 5242880
-
+char * docroot;
+int rd;
 
 void handle_connection(int acceptfd) {
 	FILE *file = NULL;
     char buf[1024];
     long read;
+    char *token[1024];
     char *msg, *fpath, *v;
     read = recv(acceptfd, buf, 1024, 0);
     buf[read] = '\0';
@@ -27,19 +29,90 @@ void handle_connection(int acceptfd) {
     fpath = ' ';
     v = ' ';
     strncpy(msg, buf, sizeof buf);
-    msg = strtok(msg, ' ');
-    fpath = strtok(fpath, ' ');    
-	v = strtok(v, '\r\n');
-    printf("%s\n", msg);
-    printf("%s\n", fpath);
-    printf("%s\n", v);
-
-    file = fopen(fpath, 'r');
-    if (file == NULL){
-        exit(0); //file not found
+    if (read < 0)
+    {
+        fprintf(stderr, ("recv() error\n"));
     }
+    else if (!read)
+    {
+        fprintf(stderr, ("client disconnect\n"));
+    }
+    else
+    {
+        
+        printf("%s", msg);
+        token[0] = strtok(msg, " \t\n");        
 
-    fclose(file);
+        if ((strncmp(token[0], "GET\0", 4) == 0) || (strncmp(token[0], "HEAD\0", 5) == 0)) {
+            
+            token[1] = strtok (NULL, " \t");
+            token[2] = strtok (NULL, " \t\n");
+
+            if (strncmp(token[1], "HTTP/1.1", 8) != 0 && strncmp(token[2], "HTTP/1.1", 8) != 0) {
+                write(acceptfd, "HTTP/1.1 400 Bad Requests\n", 25);
+            }
+            else { 
+                
+                strncpy(fpath, docroot, sizeof docroot);
+                strncpy(&fpath[strlen(docroot)], token[1], sizeof token[1]);
+
+                if ((file = open(fpath, O_RDONLY)) != -1 ) { //this means the file exists
+
+                    write(acceptfd, "HTTP/1.1 200 OK\r\n", 17);
+                    write(acceptfd, "Content-Type: ", 14);
+
+                    char * extenstion = strrchr(fpath, '.');
+
+                    if(strncmp(extenstion, ".jpg", 4) == 0){
+                        write(acceptfd, "image/jpeg\r\n", 12);
+                    }
+                    
+                    else if(strncmp(extenstion, ".png", 4) == 0){
+                        write(acceptfd, "image/png\r\n", 11);
+                    }
+
+                    else if(strncmp(extenstion, ".gif", 4) == 0){
+                        write(acceptfd, "image/gif\r\n", 11);
+                    }
+
+                    else if(strncmp(extenstion, ".pdf", 4) == 0){
+                        write(acceptfd, "application/pdf\r\n", 17);
+                    }
+
+                    else if(strncmp(extenstion, ".js", 3) == 0){
+                        write(acceptfd, "application/javascript\r\n", 24);
+                    }
+
+                    else if(strncmp(extenstion, ".html", 5) == 0){
+                        write(acceptfd, "text/html\r\n", 11);
+                    }
+
+                    else if(strncmp(extenstion, ".txt", 4) == 0){
+                        write(acceptfd, "text/plain\r\n", 12);
+                    }
+
+                    else if(strncmp(extenstion, ".css", 4) == 0){
+                        write(acceptfd, "text/css\r\n", 10);
+                    }
+
+                    else {
+                        write(acceptfd, "\r\n", 2);
+                    }
+
+                    //todo get dates
+
+                    //todo content length
+
+                    //while ((rd = read(file, buf, BYTES)) > 0){
+                        //load file
+                    //}
+
+
+                }
+            } 
+
+        }
+    }
 }
 
 
@@ -77,7 +150,6 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-
     int tmp_ret = bind(socket_desc, address_resource->ai_addr, address_resource->ai_addrlen);
     if (tmp_ret != 0){
         printf("ERROR: %s\n", strerror(errno));
@@ -93,7 +165,6 @@ int main(int argc, char **argv) {
 
 	while(1) {
 		accept_desc = accept(socket_desc, (struct sockaddr *) &remote_addr, &remote_addr_s);
-
 		if (!fork()) {
 			// child process
 			handle_connection(accept_desc);
